695189c1c400271b13f4f6de014dcd599d74bfe9
dejan1990 <dejan.simo@hotmail.com>
2021-05-10 14:19:43 +0200

    Make and implement middleware, logout and page redirection for admin

--- routes/web.php
+++ routes/web.php
@@ -6,46 +6,28 @@ use App\Http\Controllers\QuizController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('admin.index');
-});
-
-Auth::routes();
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false
+]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('quiz', [QuizController::class, 'index'])->name('quiz.index');
-Route::get('quiz/create', [QuizController::class, 'create'])->name('quiz.create');
-Route::post('quiz', [QuizController::class, 'store'])->name('quiz.store');
-Route::get('quiz/{quiz:slug}/edit', [QuizController::class, 'edit'])->name('quiz.edit');
-Route::put('quiz/{quiz}', [QuizController::class, 'update'])->name('quiz.update');
-Route::delete('quiz/{quiz}', [QuizController::class, 'destroy'])->name('quiz.destroy');
+Route::group(['middleware' => 'isAdmin'], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
 
-Route::get('/quiz/{quiz:slug}/questions', [QuizController::class, 'question'])->name('quiz.question');
+    Route::resource('quiz', QuizController::class)->except(['edit']);
+    Route::get('quiz/{quiz:slug}/edit', [QuizController::class, 'edit'])->name('quiz.edit');
 
-/*
-Route::get('question', [QuestionController::class, 'index'])->name('question.index');
-Route::get('question/create', [QuestionController::class, 'create'])->name('question.create');
-Route::get('question/{question:slug}', [QuestionController::class, 'show'])->name('question.show');
-Route::post('question', [QuestionController::class, 'store'])->name('question.store');
-Route::get('question/{question:slug}/edit', [QuestionController::class, 'edit'])->name('question.edit');
-Route::put('question/{question}', [QuestionController::class, 'update'])->name('question.update');
-Route::delete('question/{question}', [QuestionController::class, 'destroy'])->name('question.destroy');*/
+    Route::get('/quiz/{quiz:slug}/questions', [QuizController::class, 'question'])->name('quiz.question');
 
-Route::resource('question', QuestionController::class)->except(['show', 'edit']);
-Route::get('question/{question:slug}', [QuestionController::class, 'show'])->name('question.show');
-Route::get('question/{question:slug}/edit', [QuestionController::class, 'edit'])->name('question.edit');
+    Route::resource('question', QuestionController::class)->except(['show', 'edit']);
+    Route::get('question/{question:slug}', [QuestionController::class, 'show'])->name('question.show');
+    Route::get('question/{question:slug}/edit', [QuestionController::class, 'edit'])->name('question.edit');
 
-Route::resource('user', UserController::class)->except(['edit']);
-Route::get('user/{user:slug}/edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::resource('user', UserController::class)->except(['edit']);
+    Route::get('user/{user:slug}/edit', [UserController::class, 'edit'])->name('user.edit');
+});
